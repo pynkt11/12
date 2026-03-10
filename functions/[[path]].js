c77558e5fca2408d0da68ebdb8a0857b833de0e8
pynkt11 <53953759+pynkt11@users.noreply.github.com>
2026-03-10 17:17:13 +0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,20 +1,6 @@
 export async function onRequest(context) {
-  const { request } = context;
-  const url = new URL(request.url);
-  const host = url.hostname;
-  
-  // 1. Если это главный домен - работаем как обычно
-  if (host === 'cgcasino.app' || host === 'www.cgcasino.app') {
-    return context.next();
-  }
-
-  // 2. Вытаскиваем поддомен
-  const sub = host.split('.')[0];
-  
-  // 3. Формируем путь к файлу в папке sites
-  const newPath = `/sites/${sub}/index.html`;
-
-  // 4. ДЕЛАЕМ ПЕРЕНАПРАВЛЕНИЕ (внутреннее)
-  // Мы используем fetch, чтобы "подтянуть" содержимое из нужной папки
-  return fetch(new Request(new URL(newPath, url.origin), request));
+  return new Response("SCRIPT IS RUNNING", {
+    status: 200,
+    headers: { "content-type": "text/plain" }
+  });
 }
