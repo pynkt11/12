d06804be536264e7a376308407258c01f4ebdff5
pynkt11 <53953759+pynkt11@users.noreply.github.com>
2026-03-10 16:45:38 +0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,15 +1,20 @@
 export async function onRequest(context) {
   const { request } = context;
   const url = new URL(request.url);
-  const sub = url.hostname.split('.')[0];
+  const host = url.hostname;
+  const sub = host.split('.')[0];
 
-  // Если это главный домен - отдаем стандартно
-  if (sub === 'cgcasino' || sub === 'www' || sub === 'app') {
+  // Если это главный домен - отдаем как обычно
+  if (sub === 'cgcasino' || sub === 'www' || sub === '12-c54') {
     return context.next();
   }
 
-  // Внутренний роутинг: просто переписываем запрос
-  url.pathname = `/sites/${sub}${url.pathname === '/' ? '/index.html' : url.pathname}`;
-  
-  return fetch(new Request(url, request));
+  // СОБИРАЕМ ПУТЬ
+  const targetPath = `/sites/${sub}/index.html`;
+
+  // ДИАГНОСТИКА: выводим путь прямо в браузер, если сайт не грузится
+  return new Response(`Ищу файл по пути: ${targetPath} | Хост: ${host}`, {
+    status: 404,
+    headers: { "content-type": "text/plain; charset=utf-8" }
+  });
 }
