6d0d64dfdbb29e6e73829e28e2482f6b2afd0cdc
pynkt11 <53953759+pynkt11@users.noreply.github.com>
2026-03-10 16:49:47 +0700

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -4,17 +4,15 @@ export async function onRequest(context) {
   const host = url.hostname;
   const sub = host.split('.')[0];
 
-  // Если это главный домен - отдаем как обычно
+  // 1. Если это главный домен - отдаем как обычно
   if (sub === 'cgcasino' || sub === 'www' || sub === '12-c54') {
     return context.next();
   }
 
-  // СОБИРАЕМ ПУТЬ
-  const targetPath = `/sites/${sub}/index.html`;
+  // 2. Если зашли на поддомен - переписываем путь
+  // Мы делаем это ВНУТРИ Cloudflare, пользователь не увидит /sites/ в браузере
+  url.pathname = `/sites/${sub}${url.pathname === '/' ? '/index.html' : url.pathname}`;
 
-  // ДИАГНОСТИКА: выводим путь прямо в браузер, если сайт не грузится
-  return new Response(`Ищу файл по пути: ${targetPath} | Хост: ${host}`, {
-    status: 404,
-    headers: { "content-type": "text/plain; charset=utf-8" }
-  });
+  // 3. Возвращаем контент по новому пути
+  return fetch(new Request(url, request));
 }
